85c366af5ec43d532db6be47ebb1ab26c3a87191
imohmia <imohmia@gmail.com>
2024-12-27 04:39:44 +0400

    Update db.js

--- db.js
+++ db.js
@@ -6,6 +6,26 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false, // Enable SSL for secure connections
+    },
+});
+
+// Test database connection
+pool.connect((err) => {
+    if (err) {
+        console.error('Error connecting to PostgreSQL database:', err);
+    } else {
+        console.log('Connected to PostgreSQL database!');
+    }
+});
+
+// Graceful shutdown to close DB connections
+process.on('SIGINT', async () => {
+    console.log('Closing PostgreSQL connection pool...');
+    await pool.end();
+    console.log('PostgreSQL connection pool closed.');
+    process.exit(0);
 });
 
 module.exports = pool;
